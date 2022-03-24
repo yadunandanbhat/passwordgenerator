25e7dde17930631ec450e02817e56295b387a6b9
Yadunandan Bhat <yadunandanbhat@gmail.com>
2022-03-24 23:17:48 +0530

    Inital script setup

---
+++ src/index.js
@@ -0,0 +1,22 @@
+var generator = require('generate-password');
+var passwordlength = document.getElementById('length');
+var ifnumbers = document.getElementById('putnum');
+var ifsymbols = document.getElementById('putsym');
+var iflower = document.getElementById('putlower');
+var ifupper = document.getElementById('putupper');
+var exclsim = document.getElementById('excludesimilar');
+var excludechar = document.getElementById('exclude');
+var ifstrict = document.getElementById('strict');
+
+var password = generator.generate({
+    length: passwordlength,
+    numbers: ifnumbers.checked ,
+    symbols: ifsymbols.checked,
+    lowercase: iflower.checked,
+    uppercase: ifupper.checked,
+    excludeSimilarCharacters: exclsim.checked,
+    exclude: excludechar,
+    strict: ifstrict.checked
+});
+
+document.getElementById('password').innerHTML = password;
