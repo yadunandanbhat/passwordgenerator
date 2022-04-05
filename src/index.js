f99c7eb9bcc6acbc12d6d5a1f43ce67d2d573245
Yadunandan Bhat <yadunandanbhat@gmail.com>
2022-04-05 23:02:01 +0530

    Added webpack

--- src/index.js
+++ src/index.js
@@ -1,23 +1,23 @@
-import { generate } from 'generate-password';
+var generator = require('generate-password-browser');
 
-var passlen = document.getElementById('length');
-var putnum = document.querySelector('#putnum');
-var putsym = document.querySelector('#putsym');
-var putlower = document.querySelector('#putlower');
-var putupper = document.querySelector('#putupper');
-var exclsim = document.querySelector('#exclduesimilar');
-var str = document.querySelector('#strict');
+var passlen = document.getElementById("length");
+var putnum = document.querySelector("#putnum");
+var putsym = document.querySelector("#putsym");
+var putlower = document.querySelector("#putlower");
+var putupper = document.querySelector("#putupper");
+var exclsim = document.querySelector("#exclduesimilar");
+var str = document.querySelector("#strict");
 
-var submt = document.querySelector('#submit');
-submt.addEventListener('click', (event)=> {
-    var passwd = generate({
+var submt = document.querySelector("#submit");
+submt.addEventListener("click", (event) => {
+    var passwd = generator.generate({
         length: 10,
         numbers: putnum.checked,
         symbols: putsym.checked,
         lowercase: putlower.checked,
         uppercase: putupper.checked,
-        excludeSimilarCharacters: exclsim.checked,
-        strict: str.checked
+        // excludeSimilarCharacters: exclsim.checked,
+        strict: str.checked,
     });
-    document.getElementById("password").innerHTML(passwd);
-})
+    document.getElementById("password").innerHTML = passwd;
+});
