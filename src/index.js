6befd4a3efd763c64fd02835af04aed585f8b59a
Yadunandan Bhat <yadunandanbhat@gmail.com>
2022-04-07 22:54:22 +0530

    Added exclude

--- src/index.js
+++ src/index.js
@@ -1,11 +1,12 @@
 var generator = require('generate-password-browser');
 
-var passlen = document.getElementById("length").value;
+const passlen = document.getElementById("length").value;
 var putnum = document.querySelector("#putnum");
 var putsym = document.querySelector("#putsym");
 var putlower = document.querySelector("#putlower");
 var putupper = document.querySelector("#putupper");
 var exclsim = document.querySelector("#exclduesimilar");
+const excl = document.getElementById("exclude").value;
 var str = document.querySelector("#strict");
 
 var submit = document.querySelector("#submit");
@@ -19,6 +20,7 @@ function passgen() {
         lowercase: putlower.checked,
         uppercase: putupper.checked,
         excludeSimilarCharacters: exclsim.checked,
+        exclude: excl,
         strict: str.checked,
     });
 }
